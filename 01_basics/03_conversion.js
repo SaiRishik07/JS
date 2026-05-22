f28623832f864cc62a906be067b5b1de404c2ddd
SaiRishik07 <sairishikkoli7@gmail.com>
2026-05-22 11:50:25 +0530

    comparisons

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -5,3 +5,18 @@ console.log(typeof(score));
 let valueInNumber = Number(score)
 
 console.log(typeof valueInNumber);
+
+
+// ************************ Operations ******************** 
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+let str1 = "hello "
+let str2 = "sai"
+
+let gameCounter = 300
+++gameCounter;
+console.log(gameCounter);
+
+
